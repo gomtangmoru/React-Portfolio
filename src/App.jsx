cd350ae5e141bb1f3b828cf7f4f46cb1f0ad3eed
Ryu Jae Young <gomtangmoru@mueslicorp.com>
2024-04-04 14:15:21 +0900

    중간 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import lang_en from './/lang/en-US.json';import lang_ko from './/lang/ko-KR.json';
+
+
+/*function language_init(){
+    let lang = navigator.language || navigator.userLanguage;
+    if(lang === 'ko-KR'){
+        transKorean()
+    }else{
+        return 'en';
+    }
+}*/
+function transKorean(){
+    console.log(lang_ko,lang_en)
+}
+
 
 function Index() {
     useEffect(() => {
         document.title = "뿌직";
+
     }, []);
     return (
         <div className={'container'}>
-{/*            <p>container</p>*/}
+
+            {/*            <p>container</p>*/}
             <div className={'banner'}>
                 {/*<div className={'header'}>*/}
                 {/*    <p>header</p>*/}
                 {/*</div>*/}
+            </div>
+            <div className={'navbar'}>
+                <a onClick={transKorean}>한국어</a>
             </div>
             <div className={'name-blur'}></div>
 
@@ -19,10 +39,14 @@ function Index() {
                 <div className={'name'}>
                     <h1>JaeYoung Ryu</h1>
                 </div>
-                <h2 className={'title'}>Hi! 👋</h2>
+
+
                 <div className={'profile'}></div>
             </div>
-            <div className={'longbox'}></div>
+            <div className={'longbox'}>
+                <h2 className={'title'}>Hi! 👋</h2>
+                <p className={'index'}>INDEX TEST <br/> BR TEST</p>
+            </div>
         </div>
     );
 }

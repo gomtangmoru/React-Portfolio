09032d55ef5d96e22e0974317bef611d6b82535c
Ryu Jae Young <gomtangmoru@mueslicorp.com>
2024-04-04 16:57:07 +0900

    language support update

--- src/App.jsx
+++ src/App.jsx
@@ -2,65 +2,50 @@ import React, { useEffect } from 'react';
 import './App.css';
 import lang_en from './/lang/en-US.json';import lang_ko from './/lang/ko-KR.json';
 
-
-function language_init(){
-    let lang = navigator.language || navigator.userLanguage;
-    if(lang === 'ko-KR'){
-        transKorean()
-    }else{
-        transEnglish()
+function translang(langinfo) {
+    if (langinfo === 'ko-KR') {
+        console.log(lang_ko.title, lang_ko)
+        document.title = lang_ko.title
+        document.querySelector('.index').innerHTML = lang_ko.index
+        document.querySelector('.title').innerHTML = lang_ko.title}
+    else {
+        console.log(lang_en.title, lang_en)
+        document.title = lang_en.title
+        document.querySelector('.index').innerHTML = lang_en.index
+        document.querySelector('.title').innerHTML = lang_en.title
     }
-}
-function transKorean(){
-    console.log(lang_ko.title,lang_ko)
-    document.title = lang_ko.title
-    document.querySelector('.index').innerHTML = lang_ko.index
-    document.querySelector('.title').innerHTML = lang_ko.title
-}
-function transEnglish(){
-    console.log(lang_en.title,lang_en)
-    document.title = lang_en.title
-    document.querySelector('.index').innerHTML = lang_en.index
-    document.querySelector('.title').innerHTML = lang_en.title
-}
+};
 
 function Index() {
     useEffect(() => {
         document.title = "뿌직";
-        language_init()
+        let lang = navigator.language || navigator.userLanguage;
+        translang(lang);
     }, []);
     return (
         <div className={'container'}>
-
-            {/*            <p>container</p>*/}
-            <div className={'banner'}>
-                {/*<div className={'header'}>*/}
-                {/*    <p>header</p>*/}
-                {/*</div>*/}
-            </div>
+            <div className={'banner'}></div>
             <div className={'navbar'}>
-                <p onClick={transKorean}>한국어</p>
-                <p></p>
-                <p onClick={transEnglish}>English</p>
+                <p onClick={() => translang("ko-KR")}>한국어</p>
+                <p>     </p>
+                <p onClick={() => translang("en-US")}>English</p>
             </div>
             <div className={'name-blur'}></div>
-
             <div className={'profile-circle-cover'}>
                 <div className={'name'}>
                     <h1>JaeYoung Ryu</h1>
                 </div>
-
-
                 <div className={'profile'}></div>
             </div>
             <div className={'longbox'}>
                 <h2 className={'title'}>Hd! 👋</h2>
             </div>
             <p className={'index'}>INDEX TEST <br/> BR TEST</p>
-
         </div>
     );
-}
+};
 
 export default Index;
+
+
 // render(<Desktop1/>, document.getElementById('root'));
